6c816b2ffaac1177cdf930e62efdd592ff099ebf
Watanabeharuto5 <karismaavibra@gmail.com>
2026-04-23 14:55:56 +0700

    menambahkan app_.dart

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,24 @@
+import '../../main.dart';
+import 'package:flutter/material.dart';
+import '../../../features/auth/presentation/pages/login_page.dart';
+import '../../../features/auth/presentation/pages/register_page.dart';
+import '../../../features/auth/presentation/pages/verify_email_page.dart';
+import '../../../features/dashboard/presentation/pages/dashboard_page.dart';
+import '../guards/authguard.dart';
+
+class AppRouter {
+  static const String splash      = '/';
+  static const String login       = '/login';
+  static const String register    = '/register';
+  static const String verifyEmail = '/verify-email';
+  static const String dashboard   = '/dashboard';
+
+
+  static Map<String, WidgetBuilder> get routes => {
+    splash:      (_) => const SplashPage(),
+    login:       (_) => const LoginPage(),
+    register:    (_) => const RegisterPage(),
+    verifyEmail: (_) => const VerifyEmailPage(),
+    dashboard:   (_) => const AuthGuard(child: DashboardPage()),
+  };
+}
