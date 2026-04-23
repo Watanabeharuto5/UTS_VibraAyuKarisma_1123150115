52edc33c72828cfb0af261d121ef88cddb08a41e
Watanabeharuto5 <karismaavibra@gmail.com>
2026-04-23 14:02:25 +0700

    menambahkan auth_provider.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
+
 import 'firebase_options.dart';
 
+import './features/auth/presentation/providers/auth_provider.dart';
+import './features/dashboard/presentation/providers/product_provider.dart';
+
+import './core/theme/app_theme.dart';
+import './core/service/secure_storage.dart';
+import './core/routes/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-
-  // Inisialisasi Firebase SEBELUM runApp
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  runApp(const MyApp());
+}
 
-  runApp(
-    MultiProvider(
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
       ],
-      child: const MyApp(),
-    ),
-  );
+      child: MaterialApp(
+        title: 'My App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        initialRoute: AppRouter.splash,
+        routes: AppRouter.routes,
+      ),
+    );
+  }
+}
+
+// Splash Page
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (!mounted) return;
+
+    final token = await SecureStorageService.getToken();
+
+    final route = token != null
+        ? AppRouter.dashboard
+        : AppRouter.login;
+
+    Navigator.pushReplacementNamed(context, route);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
 }
